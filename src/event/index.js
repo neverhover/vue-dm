e4cd9745bff6885a47e0bd4bb2fd396cb8b2b1e2
leyizhang <leyizhang@ligowave.com>
2017-07-12 22:31:41 +0800

    添加事件总线和存储目录

---
+++ src/event/index.js
@@ -0,0 +1,7 @@
+/**
+ * Created by blood on 2017/7/12.
+ */
+import Vue form 'vue';
+export default new Vue({
+
+});
---
+++ src/store/index.js
@@ -0,0 +1,11 @@
+/**
+ * Created by blood on 2017/7/12.
+ */
+import Vue from 'vue';
+import Vuex from 'vuex';
+
+Vue.use(Vuex)
+
+export default new Vuex.Store({
+
+})
